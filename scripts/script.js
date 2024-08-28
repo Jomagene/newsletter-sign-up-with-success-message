d792165323b0af824f2797480ad7a224372c79e7
Jomagene <semjomagene@gmail.com>
2024-08-28 06:07:27 +0200

    Implement email validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,29 @@
-let Myform = document.getElementById("my-form");
+const myForm = document.getElementById("my-form");
+let formInput = document.querySelector("input[name='myMail']");
+let labelInput = document.querySelector("input");
+let errorContainer = document.getElementById("error");
+
+function validateEmail() {
+  const email = new FormData(myForm).get("myMail");
+  console.log(email);
+  if (!email) return "Valid Email required";
+
+  const isValidEmail = /^\S+@\S+$/g;
+  if (!isValidEmail.test(email)) {
+    return "Please enter a valid email";
+  }
+
+  return "";
+}
 
 function handleFormSubmit(event) {
   event.preventDefault();
-  location.href = "success.html";
+
+  const errorMessage = validateEmail();
+  if (errorMessage) {
+    formInput.classList.add("fail");
+    errorContainer.textContent = errorMessage;
+    errorContainer.classList.add("fail");
+  } else location.href = "success.html";
 }
-Myform.addEventListener("submit", handleFormSubmit);
+myForm.addEventListener("submit", handleFormSubmit);
